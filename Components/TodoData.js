29f37c5b206f33d187cb8ecae3fc7934e1e96cf6
j-jh <greenjello106@gmail.com>
2025-09-28 19:55:22 -0700

    feat (TodoData): fetch data and pass to TodoList

--- Components/TodoData.js
+++ Components/TodoData.js
@@ -1,26 +1,33 @@
-"use client"
-import TodoList from "./TodoList"
-import { useState, useEffect } from "react"
+"use client";
+import TodoList from "@/Components/TodoList";
+
+import { useState, useEffect } from "react";
+
 export default function TodoData() {
-    const [data, setData] = useState([]);
-    
+    const [todoObj, setTodoObj] = useState([]);
     useEffect(() => {
-        async function fetchData() {
+        fetchData()
+    }, []);
+
+    async function fetchData() {
         try {
-            const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+            const response = await fetch(`https://jsonplaceholder.typicode.com/todos`);
             if (!response.ok) {
-                console.log("Error fetching");
+                console.log("Failed to fetch");
                 return;
             }
             const result = await response.json();
+            setTodoObj(result);
             console.log(result);
-            setData(result);
         } catch (error) {
-            console.log(error);
+            console.log(error.message);
         }
-    } fetchData()}, []);
+    };
+    console.log("test");
 
     return (
-        <TodoList todos={data || []}/>
-    )
+        <div>
+            <TodoList todos={todoObj}/>
+        </div>
+    );
 }
