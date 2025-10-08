3bd79abdb8a21ad2cc3a94c4af1a1d67b25c6021
j-jh <greenjello106@gmail.com>
2025-10-07 21:17:51 -0700

    feat: add todo and update state

--- Components/AddTodo.js
+++ Components/AddTodo.js
@@ -1,16 +1,18 @@
 "use client";
 import { useState } from "react";
-export default function AddTodo({ setTodoObj }) {
+export default function AddTodo({ userIdList, setTodoObj, setUserIdList }) {
     const [formData, setFormData] = useState({ userId: "", title: "" });
 
-    async function fetchPost() {
+    // restrict userid input to only nums
+
+    async function fetchPost(title, currUserId) {
         try {
             const request = await fetch(`https://jsonplaceholder.typicode.com/todos`,
                 {
                     method: `POST`,
                     body: JSON.stringify({
-                        userId: "",
-                        title: "",
+                        userId: currUserId,
+                        title: title,
                         completed: false
                     })
                 }
@@ -26,8 +28,8 @@ export default function AddTodo({ setTodoObj }) {
                 prev => ([
                     ...prev,
                     {
-                        userId: formData.userId,
-                        title: formData.title,
+                        userId: currUserId,
+                        title: title,
                         completed: false,
                         id: Date.now()
                     }
@@ -41,24 +43,47 @@ export default function AddTodo({ setTodoObj }) {
 
     function handleFormSubmit(e) {
         e.preventDefault();
-        const formObj = new FormData(e.target);
+        let currUserId = formData.userId
+        if (currUserId === "New User") {
+            currUserId = userIdList.length+1;
+
+            setFormData(prev => ({
+                ... prev, userId: currUserId
+            }))
+            setUserIdList(prev => [... prev, currUserId]);
+            console.log(userIdList);
+        }
         // console.log(formObj);
-        fetchPost();
+        fetchPost(formData.title, Number(currUserId));
+        clearForm(false);
     }
 
-    function clearForm() {
-        setFormData({ userId: "", title: "" });
+    function clearForm(clearAll) {
+        if (clearAll) {
+            setFormData({userId: "", title: "" });
+        }
+        setFormData(prev => ({... prev, title: "" }));
+    }
+
+    function handleIdSelect(e) {
+        
+        setFormData(prev => ({ ...prev, userId: e.target.value }))
+        console.log(e.target.value);
     }
 
     return (
         <div>
             <form onSubmit={handleFormSubmit}>
-                <input name="userId"
-                    value={formData.userId}
-                    onChange={
-                        e => setFormData(prev =>
-                            ({ ...prev, userId: e.target.value })
-                        )} />
+                <label>| User ID: </label>
+                <select value={formData.userId} onChange={handleIdSelect}>
+                    {userIdList.map(
+                        item => <option key={item} value={item}>
+                            {item}
+                        </option>
+                    )}
+                    <option key={userIdList.length} value={"New User"}>New User</option>
+                </select>
+                <label>| Item: </label>
                 <input name="title"
                     value={formData.title}
                     onChange={
@@ -66,7 +91,7 @@ export default function AddTodo({ setTodoObj }) {
                             ({ ...prev, title: e.target.value })
                         )} />
                 <button type="submit">Add</button>
-                <button onClick={clearForm}>Clear</button>
+                <button type="button" onClick={e => clearForm(true)}>Clear</button>
             </form>
         </div>
     )
