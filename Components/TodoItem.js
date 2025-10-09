6b87af0c04c4c88b55b946c60154d757cb3557b1
j-jh <greenjello106@gmail.com>
2025-10-08 22:08:25 -0700

    fix: styling, props

--- Components/TodoItem.js
+++ Components/TodoItem.js
@@ -1,13 +1,27 @@
-export default function TodoItem({ todo }) {
 
-    const { userId, title, completed } = todo;
+import DeleteTodo from "./DeleteTodo";
+import EditTodo from "./EditTodo";
 
+export default function TodoItem({ todo, showEditOptions }) {
+
+    const { id, userId, title, completed } = todo;
 
     return (
-        <tr>
-            <td>| {userId} </td> 
-            <td>| {title}</td> 
-            <td>| {completed ? "✅" : "❌"} |</td>
-        </tr>
+        <>
+            <tr>
+                <td>| {userId} </td>
+                <td>| {title} </td>
+                <td>| {completed ? "✅" : "❌"} |</td>
+                <td><DeleteTodo idToDelete={id} showEditOptions={showEditOptions} />
+                    <EditTodo idToDelete={id} showEditOptions={showEditOptions} />
+                </td>
+            </tr>
+            <tr>
+                <td>------------</td>
+                <td>----------------------------------------------------------------------------------------------------</td>
+                <td>----------</td>
+                <td> </td>
+            </tr >
+        </>
     )
 }
